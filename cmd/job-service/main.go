187ee97f11c865896135dc90e47062b78f68802a
Pouya <pouyamohammadyirbu@gmail.com>
2025-04-03 21:05:37 +0330

    feat: implementing base for job-service logic

--- cmd/job-service/main.go
+++ cmd/job-service/main.go
@@ -1,3 +1,32 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+
+	"github.com/pm-cloudify/job-service/internal/service"
+	"github.com/pm-cloudify/job-service/pkg/config_loader"
+	"github.com/pm-cloudify/job-service/pkg/mb"
+)
+
+func main() {
+	config_loader.LoadEnv("./configs")
+
+	var (
+		addr   = os.Getenv("RMQ_ADDR")
+		user   = os.Getenv("RMQ_USER")
+		pass   = os.Getenv("RMQ_PASS")
+		q_name = os.Getenv("RMQ_Q_NAME")
+	)
+
+	app_mb, err := mb.InitMessageBroker(addr, user, pass, q_name)
+	if err != nil {
+		log.Fatalf("Failed to initialize message broker: %v", err)
+	}
+	defer app_mb.Close()
+
+	err = mb.RunConsumer(app_mb, service.JobService)
+	if err != nil {
+		log.Fatalf("Failed to run consumer: %v", err)
+	}
+}
---
+++ internal/service/job.go
@@ -0,0 +1,7 @@
+package service
+
+func JobService(data []byte) {
+	// TODO:
+	// 1 - read and compile give data
+	// 2 - create a job and add it to database
+}
