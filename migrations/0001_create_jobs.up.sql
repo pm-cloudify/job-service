70591df54309e62d9dabcf6535c1d80bfd3dc15f
Pouya <pouyamohammadyirbu@gmail.com>
2025-04-03 21:53:29 +0330

    chore: removing migration files from this project, it is handled in core project

--- migrations/0001_create_jobs.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TYPE job_status AS ENUM ('created', 'executing', 'done', 'failed');
-CREATE TABLE jobs (
-    id SERIAL PRIMARY KEY,
-    file_id INT NOT NULL,
-    status job_status NOT NULL DEFAULT 'created',
-    query VARCHAR(512),
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    last_change TIMESTAMP,
-);
